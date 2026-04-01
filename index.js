edea4cfc6b55b0c60bbff73a94e1846de29b57a0
WildFont <jordifont4@gmail.com>
2026-04-01 17:02:07 +0200

    just playing with js data types

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const s5 = "and";
 
 
 // Print out the concatenated string
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`
+console.log(tongueTwister);
 
 
 
@@ -21,11 +22,15 @@ const s5 = "and";
 const part1 = "java";
 const part2 = "script";
 
+
+
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const result = (part1.slice(0, -1) + part1[part1.length - 1].toUpperCase()) + (part2.slice(0, -1) + part2[part2.length - 1].toUpperCase());
+
 
 
 // Print the cameLtaiL-formatted string
-
+console.log(result)
 
 
 
@@ -36,9 +41,10 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
-
+const tipAmount = (billTotal * 0.15) + billTotal;
 // Print out the tipAmount
 
+console.log(tipAmount);
 
 
 
@@ -47,10 +53,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.random() * 10;
+//const integerRandomNumber = Math.round (randomNumber);
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -61,16 +68,16 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; // false
 
-const expression2 = a || b;
+const expression2 = a || b; // true
 
-const expression3 = !a && b;
+const expression3 = !a && b; // false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); //false
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; // true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); // true
 
-const expression7 = a && a;
+const expression7 = a && a; // true
